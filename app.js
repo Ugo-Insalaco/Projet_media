d8928272fb5fdcae62b3b8ed3334aa4fe621256f
zouaoui <ilies.zouaoui@ecl19.ec-lyon.fr>
2021-03-11 19:14:58 +0100

    Ajout des balises audio et video et d'éléments de test

--- app.js
+++ app.js
@@ -9,12 +9,18 @@ app.use('/private', express.static(path.join(__dirname, './srv/private')))
 app.set('views',path.join(__dirname, './srv/private/views'))
 
 app.get('/', function(req, res){
-    res.render('media.ejs',{id:765})
+    res.render('media.ejs', { id: 765 })
 })
 
 app.get('/:id',function(req, res){
     const image = fs.readFileSync('./medias/oeuf.jpg')
     res.send(image)
 })
+
+app.get('/:vid', function (req, res) {
+    const video = fs.readFileSync('./medias/video.mp4')
+    res.send(video)
+})
+
 console.log('lancement du serveur')
 app.listen(3000)
