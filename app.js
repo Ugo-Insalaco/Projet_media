669fc8c20d5f874f1e671c9572b75aea11d8e152
ugo-insalaco <ugo.insalaco@gmail.com>
2021-03-17 12:41:27 +0100

    avancement 17/03/2020

--- app.js
+++ app.js
@@ -12,5 +12,36 @@ app.get('/', function(req, res){
     res.render('media.ejs', { id: 765 })
 })
 
+app.get('/video', function(req, res){
+    const filenames = fs.readdirSync(path.join(__dirname,'/srv/public/video'))
+    res.send(filenames)
+})
+
+const recupfichier = function(chemin){
+    // récupération nom fichiers
+    const filenames= fs.readdirSync(path.join(__dirname,`/srv/public/${chemin}`))
+
+    // Pour chaque fichier
+    if(filenames.length>0){
+        for (let i=0; i<filenames.length; i++)
+        // SI c'est un dossier
+        if (filenames[i].split('.').length===1){
+
+            app.get(`${chemin}/${filenames[i]}`, function(req, res){
+                const filenames2 = fs.readdirSync(path.join(__dirname,`/srv/public/${chemin}/${filenames[i]}`))
+                res.send(filenames2)
+            })
+            recupfichier(`${chemin}/${filenames[i]}`)
+        }
+        return 
+    }
+    else{
+        return 
+    }
+}
+
+recupfichier('/video')
+
+
 console.log('lancement du serveur')
 app.listen(3000)
--- srv/private/js/media.js
+++ srv/private/js/media.js
@@ -1,1 +1,77 @@
 console.log('la page js est chargée')
+
+const updateChemin = function(e){
+    let chemin = e.currentTarget.innerHTML
+    url = $('#chemin').html()
+    if(chemin.split('.').length>1){
+        $("#video_player").attr("src",`${url}/${chemin}`)   
+    }
+    else{
+        $.ajax({
+           type: "GET",
+           url: `${url}/${chemin}`,
+           success: function(res){
+               $("#list").html("")
+            for (let i=0; i<res.length; i++){
+                $("#list").append(`<button id="bouton1" type="button" class="list-group-item list-group-item-action">${res[i]}</button>`)
+            }
+            $(".list-group-item").on("click", updateChemin)
+            $('#chemin').html(`${url}/${chemin}`)
+            id = `${url.split('/').slice(1).join('')}${chemin}`
+            $('#breadcrumbChemin').append(`<li id=${id} class="breadcrumb-item">${chemin}</li>`)
+           },
+           error: function(err){
+               console.log(err)
+           } 
+        })
+    }
+}
+
+const initialisation = function(){
+    $.ajax({
+        type: "GET",
+        url: "/video",
+        success: function(res){
+            for (let i=0; i<res.length; i++){
+                $("#list").append(`<button id="bouton1" type="button" class="list-group-item list-group-item-action">${res[i]}</button>`)
+            }
+            $(".list-group-item").on("click", updateChemin)
+            $('#chemin').html('/video')
+            $('#breadcrumbChemin').html('<li id=video class="breadcrumb-item">video</li>')
+        },
+        error: function(err){
+            console.log(err)
+        }
+    })
+}
+
+const retourChemin = function(e){
+    url = $('#chemin').html()
+    url= url.split('/')
+    console.log(url)
+    chemin = url[url.length -1]
+    url = url.slice(0,-1).join('/')
+    console.log(url)
+    if(url!== ''){
+        $.ajax({
+            type: "GET",
+            url: `${url}`,
+            success: function(res){
+                $("#list").html("")
+             for (let i=0; i<res.length; i++){
+                 $("#list").append(`<button id="bouton1" type="button" class="list-group-item list-group-item-action">${res[i]}</button>`)
+             }
+             $(".list-group-item").on("click", updateChemin)
+             $('#chemin').html(`${url}`)
+             id = `${url.split('/').slice(1).join('')}${chemin}`
+             $(`#${id}`).remove()
+            },
+            error: function(err){
+                console.log(err)
+            } 
+         })
+    }
+
+}
+$("#back").on("click", retourChemin)
+initialisation()
