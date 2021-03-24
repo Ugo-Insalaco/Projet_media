ad0945650a23883d775ae1e5fe4f0de3e173001c
ugo-insalaco <ugo.insalaco@gmail.com>
2021-03-24 21:51:52 +0100

    avancement 24/03

--- app.js
+++ app.js
@@ -1,33 +1,185 @@
+// Importation des packages
 const express = require('express')
 const path = require('path')
 const app = express()
 const fs = require('fs')
+const jsonParser = require('body-parser').json({
+    type: "application/vnd.api+json",
+})
+const xss = require('xss')
 
+
+// Requêtes des fichiers statiques
 app.use('/', express.static(path.join(__dirname, './srv/public')))
-
 app.use('/private', express.static(path.join(__dirname, './srv/private')))
+
+// Initialisation du dossier des views
 app.set('views',path.join(__dirname, './srv/private/views'))
 
+// Requête princpale (affichage de la page)
 app.get('/', function(req, res){
     res.render('media.ejs', { id: 765 })
 })
 
+// Revoie les nom des dossiers du dossier principal 'video'
 app.get('/video', function(req, res){
     const filenames = fs.readdirSync(path.join(__dirname,'/srv/public/video'))
     res.send(filenames)
 })
 
+app.get('/videoPlaylist', function(req,res){
+    //renvoyer la liste des noms des playlistes
+    res.send(Object.keys(playlistobj))
+})
+
+app.post('/video/getPlaylistElements', jsonParser, function(req, res){
+    playlistName = xss(req.body.playlistName)
+    if(playlistName!==undefined){
+        if(playlistobj.hasOwnProperty(playlistName)){
+            res.send(playlistobj[playlistName])
+        }
+        else{
+            res.status(404).send({
+                route: '/video/getPlaylistElements',
+                method: 'POST',
+                message: 'La playlist n\'existe pas'
+            })
+        }
+    }
+    else{
+        res.status(400).send({
+            route: '/video/getPlaylistElements',
+            method: 'POST',
+            message: 'Paramètres invalides'
+        })
+    }
+})
+
+app.post('/video/addPlaylist', jsonParser, function(req, res){
+    playlistName = xss(req.body.playlistName)
+    if(playlistName!== undefined){
+        if(playlistobj.hasOwnProperty(playlistName)){
+            res.status(404).send({
+                route: '/video/getPlaylistElements',
+                method: 'POST',
+                message: 'La playlist existe déjà'
+            })
+        }
+        else{
+            playlistobj[playlistName] = []
+            console.log(playlistobj)
+            fs.writeFile('./var/playlist_video.json',JSON.stringify(playlistobj) ,function (err) {
+                if (err) throw err;
+                else{
+                    res.status(200).send({
+                        route: '/video/getPlaylistElements',
+                        method: 'POST',
+                        message: 'La playlist a bien été ajoutée'
+                    })
+                }
+            })
+        }
+    }
+    else{
+        res.status(400).send({
+            route: '/video/getPlaylistElements',
+            method: 'POST',
+            message: 'Paramètres invalides'
+        }) 
+    }
+})
+
+app.post('/video/addPlaylistElement', jsonParser, function(req, res){
+    let playlistName = xss(req.body.playlistName)
+    let elementURL = xss(req.body.elementURL)
+    if(playlistName===undefined || elementURL===undefined){
+        res.status(400).send({
+            route: '/video/addPlaylistElement',
+            method: 'POST',
+            message: 'Paramètres invalides'
+        }) 
+    }
+    else if(!playlistobj.hasOwnProperty(playlistName)){
+        res.status(404).send({
+            route: '/video/addPlaylistElement',
+            method: 'POST',
+            message: 'La playlist n\'existe pas'
+        })
+    }
+    else if(playlistobj[playlistName].includes(elementURL)){
+        res.status(200).send({
+            route: '/video/addPlaylistElement',
+            method: 'POST',
+            message: 'L\'élément est déjà dans la playlist'
+        })
+    }
+    else{
+        playlistobj[playlistName].push(elementURL)
+        fs.writeFile('./var/playlist_video.json',JSON.stringify(playlistobj) ,function (err) {
+            if (err) throw err;
+            else{
+                res.status(200).send({
+                    route: '/video/addPlaylistElement',
+                    method: 'POST',
+                    message: 'L\'élément a bien été ajoutée'
+                })
+            }
+        })
+
+    }
+})
+
+app.delete('/video/deletePlaylistElement', jsonParser, function(req, res){
+    let playlistName = xss(req.body.playlistName)
+    let elementURL = xss(req.body.elementURL)
+    if(playlistName===undefined || elementURL===undefined){
+        res.status(400).send({
+            route: '/video/deletePlaylistElement',
+            method: 'POST',
+            message: 'Paramètres invalides'
+        }) 
+    }
+    else if(!playlistobj.hasOwnProperty(playlistName)){
+        res.status(404).send({
+            route: '/video/deletePlaylistElement',
+            method: 'POST',
+            message: 'La playlist n\'existe pas'
+        })
+    }
+    else if(!playlistobj[playlistName].includes(elementURL)){
+        res.status(200).send({
+            route: '/video/deletePlaylistElement',
+            method: 'POST',
+            message: 'L\'élément n\'est pas dans la playlist'
+        })
+    }
+    else{
+        playlistobj[playlistName].splice(playlistobj[playlistName].indexOf(elementURL),1)
+        fs.writeFile('./var/playlist_video.json',JSON.stringify(playlistobj) ,function (err) {
+            if (err) throw err;
+            else{
+                console.log(playlistobj)
+                res.status(200).send({
+                    route: '/video/deletePlaylistElement',
+                    method: 'POST',
+                    message: 'L\'élément a bien été supprimé'
+                })
+            }
+        })
+
+    }
+})
+// Récupération des chemins d'accès aux fichiers du dossier de travail et initialisation des requêtes pour chaque dossier
 const recupfichier = function(chemin){
     // récupération nom fichiers
     const filenames= fs.readdirSync(path.join(__dirname,`/srv/public/${chemin}`))
-
     // Pour chaque fichier
     if(filenames.length>0){
         for (let i=0; i<filenames.length; i++)
         // SI c'est un dossier
         if (filenames[i].split('.').length===1){
-
-            app.get(`${chemin}/${filenames[i]}`, function(req, res){
+            let url = `${chemin}/${filenames[i]}`.split(' ').join('%20')
+            app.get(url, function(req, res){
                 const filenames2 = fs.readdirSync(path.join(__dirname,`/srv/public/${chemin}/${filenames[i]}`))
                 res.send(filenames2)
             })
@@ -41,7 +193,19 @@ const recupfichier = function(chemin){
 }
 
 recupfichier('/video')
+let playlistobj = JSON.parse(fs.readFileSync('./var/playlist_video.json'))
 
+// Sauvegarde des playlist dans un fichier Json 
+const EcrirePlaylist= function(name,song){
+    obj = {}
+    obj[name]= song
+    data= JSON.stringify(obj)
+    fs.appendFile('./var/playlist_video.json',data,function (err) {
+        if (err) throw err;
+    })
+}
+
+//EcrirePlaylist('Playlist1',['LesSimpsons','futurama','american dad'])
 
 console.log('lancement du serveur')
 app.listen(3000)
--- srv/private/js/media.js
+++ srv/private/js/media.js
@@ -2,27 +2,27 @@ console.log('la page js est chargée')
 
 const updateChemin = function(e){
     let chemin = e.currentTarget.innerHTML
-    url = $('#chemin').html()
+    let url = $('#chemin').html()
     if(chemin.split('.').length>1){
         $("#video_player").attr("src",`${url}/${chemin}`)   
     }
     else{
         $.ajax({
-           type: "GET",
-           url: `${url}/${chemin}`,
-           success: function(res){
+            type: "GET",   
+            url: `${url}/${chemin}`,
+            success: function(res){
                $("#list").html("")
             for (let i=0; i<res.length; i++){
-                $("#list").append(`<button id="bouton1" type="button" class="list-group-item list-group-item-action">${res[i]}</button>`)
+                $("#list").append(`<button id="bouton${i}" type="button" draggable="true" class="list-group-item list-group-item-action">${res[i]}</button>`)
+                $(`#bouton${i}`).on("click", updateChemin)
             }
-            $(".list-group-item").on("click", updateChemin)
             $('#chemin').html(`${url}/${chemin}`)
-            id = `${url.split('/').slice(1).join('')}${chemin}`
-            $('#breadcrumbChemin').append(`<li id=${id} class="breadcrumb-item">${chemin}</li>`)
-           },
-           error: function(err){
+            id = `${url.split('/').slice(1).join('').split(' ').join('_')}${chemin.split(' ').join('_')}`
+            $('#breadcrumbChemin').append(`<li id="${id}" class="breadcrumb-item">${chemin}</li>`)
+            },
+            error: function(err){
                console.log(err)
-           } 
+            } 
         })
     }
 }
@@ -33,9 +33,9 @@ const initialisation = function(){
         url: "/video",
         success: function(res){
             for (let i=0; i<res.length; i++){
-                $("#list").append(`<button id="bouton1" type="button" class="list-group-item list-group-item-action">${res[i]}</button>`)
+                $("#list").append(`<button id="bouton${i}" type="button" draggable="true" class="list-group-item list-group-item-action">${res[i]}</button>`)
+                $(`#bouton${i}`).on("click", updateChemin)
             }
-            $(".list-group-item").on("click", updateChemin)
             $('#chemin').html('/video')
             $('#breadcrumbChemin').html('<li id=video class="breadcrumb-item">video</li>')
         },
@@ -46,24 +46,22 @@ const initialisation = function(){
 }
 
 const retourChemin = function(e){
-    url = $('#chemin').html()
+    let url = $('#chemin').html()
     url= url.split('/')
-    console.log(url)
     chemin = url[url.length -1]
     url = url.slice(0,-1).join('/')
-    console.log(url)
     if(url!== ''){
         $.ajax({
             type: "GET",
-            url: `${url}`,
+            url: `${url}`, 
             success: function(res){
                 $("#list").html("")
              for (let i=0; i<res.length; i++){
-                 $("#list").append(`<button id="bouton1" type="button" class="list-group-item list-group-item-action">${res[i]}</button>`)
+                 $("#list").append(`<button id="bouton${i}" type="button" draggable="true" class="list-group-item list-group-item-action">${res[i]}</button>`)
+                 $(`#bouton${i}`).on("click", updateChemin)
              }
-             $(".list-group-item").on("click", updateChemin)
              $('#chemin').html(`${url}`)
-             id = `${url.split('/').slice(1).join('')}${chemin}`
+             id = `${url.split('/').slice(1).join('').split(' ').join('_')}${chemin.split(' ').join('_')}`
              $(`#${id}`).remove()
             },
             error: function(err){
@@ -73,5 +71,86 @@ const retourChemin = function(e){
     }
 
 }
+
+const submitPlaylist = function(e){
+    let playlistName = $("#playlistName").val()
+    $("#add_playlist_form").slideUp()
+    $.ajax({
+        type: "POST",
+        url: '/video/addPlaylist',
+        headers: {
+            "Content-Type": "application/vnd.api+json"
+        },
+        data:JSON.stringify({playlistName: playlistName}),
+        success: function(res){
+            getVideoPlaylists()
+        },
+        error: function(err){
+            console.log(err)
+        }
+    })
+}
+
+const getVideoPlaylists = function(){
+    $.ajax({
+        type: 'GET',
+        url: '/videoPlaylist',  
+        success: function(res){
+            $("#videoPlaylist").html("")
+             for (let i=0; i<res.length; i++){
+                 $("#videoPlaylist").append(`<button id="playlistbouton${i}" type="button" draggable="true" class="list-group-item list-group-item-action">${res[i]}</button>`)
+                 $(`#playlistbouton${i}`).on('click', getPlaylistElements)
+             }
+             $("#videoPlaylist").append('<button type=button id="add_playlist" class="list-group-item list-group-item-action">Ajouter une playlist + </button>')
+             $("#add_playlist").on('click', showPlaylistForm)
+        },
+        error: function(err){
+            console.log(err)
+        }})
+}
+
+const getPlaylistElements = function(e){
+    console.log(e.currentTarget.innerHTML)
+    $.ajax({
+        type:'POST',
+        url: '/video/getPlaylistElements',
+        headers: {
+            "Content-Type": "application/vnd.api+json"
+        },
+        data: JSON.stringify({playlistName: e.currentTarget.innerHTML}),
+        success: function(res){
+            $("#videoPlaylist").html("")
+            for (let i=0; i<res.length; i++){
+                let split = res[i].split('/')
+                let titre = split.pop()
+                let url = split.join('/')
+                console.log(split, titre, url)
+                $("#videoPlaylist").append(`<button id="bouton_playlist${i}" type="button" draggable="true" class="list-group-item list-group-item-action">${titre}</button><div id="playlist_url${i}" hidden>${url}</div>`)
+                $(`#bouton_playlist${i}`).on("click", updatePlaylist)
+            }
+        },
+        errror: function(err){
+            console.log(err)
+        }
+    })
+}
+
+const updatePlaylist = function(e){
+    let id = e.currentTarget.id.split('bouton_playlist').pop()
+    url = $(`#playlist_url${id}`).html()+'/'+$(`#bouton_playlist${id}`).html()
+    $("#video_player").attr("src",`${url}`)
+}
+
+const showPlaylistForm = function(e){
+    if($("#add_playlist_form").css("display")=="block"){
+        $("#add_playlist_form").slideUp()
+    }
+    else{
+        $("#add_playlist_form").slideDown()
+    }
+}
 $("#back").on("click", retourChemin)
+$("#back_playlist").on("click", getVideoPlaylists)
+$("#submit_playlist").on('click', submitPlaylist)
 initialisation()
+getVideoPlaylists()
